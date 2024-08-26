2d8d8a7f4507fd72cc1a4a5dd009f0279c0edf68
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-08-26 10:31:18 +0600

    new file  added

--- obj.js
+++ obj.js
@@ -1,3 +1,17 @@
+const car = "color:red" +"weight:800kg" + "mcdel:Bmw"
+console.log(car)
+
 const person = {
-    
+
+     firstName : "Rakib",
+     lastname: "ahmed",
+     age:20,
+     phone: "0124587632",
+     address:"Dhaka BAngladesh"
+
+
+     
 }
+
+console.log(person.firstName,person.lastname,person.age,person.phone,person.address)
+
