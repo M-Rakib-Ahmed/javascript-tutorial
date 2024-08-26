101b49c8814315ded14b4f036d0cb403f5025cd0
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-08-26 10:14:07 +0600

    fires commit

---
+++ dataType.js
@@ -0,0 +1,8 @@
+
+
+let hablu ;
+console.log(hablu)
+console.log(typeof hablu)
+
+let bool = false;
+console.log(typeof bool)
---
+++ rakib.js
@@ -0,0 +1,29 @@
+//Automatically declared variable
+RAkib = 'Hello Bnglasesh'
+console.log(RAkib)
+rana = " Hello World "
+console.log(rana)
+
+tufan = ' hello kamal tmi koi  vai'
+console.log(tufan)
+
+//using var keyword to declared variable
+
+var x = ' helllo world hello world hello world'
+console.log(x)
+
+var y = " Hello Banglasesh Hello Bangladesh "
+console.log(y)
+
+// using let keyword to declared variables
+
+let Tutul =  ' tutul bhai'
+console.log(Tutul)
+
+let eshan =      ' hello eahan vai ami bari tmi koh'
+console.log(eshan)
+
+//using const keyword to declared variabel
+
+const ransa = ' ranasas amr mama to bon hoy ami amr bari jabo'
+console.log(ransa)
